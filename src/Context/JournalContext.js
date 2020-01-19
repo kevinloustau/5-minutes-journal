7035bcd5ee79b2590e7510194ae73b0d74f45b26
kevinloustau <kevinloustau@users.noreply.github.com>
2020-01-19 14:56:09 +0100

    Update JournalContext.js

--- src/Context/JournalContext.js
+++ src/Context/JournalContext.js
@@ -1,6 +1,31 @@
 import React from 'react'
+import uuidv4 from 'uuid/v4'
 
 export const JournalContext = React.createContext()
+
+const JournalContextValue = {
+  handleDayAdd,
+  handleDayDelete,
+  handleDaySelect,
+  handleDayUnselect,
+  handleDayChange,
+}
+
+function handleDayAdd() {
+  const id = uuidv4()
+  const newDay = {
+    id: id,
+    date: todayDate,
+    quote: `"Brilliant things happen in calm minds. Be calm. You're brilliant.Headspace." -@Headspace`,
+    gratefull: [''],
+    great: [''],
+    affirmation: '',
+    happened: [''],
+    how: '',
+  }
+  setDays([newDay, ...days])
+  setSelectedDayId(newDay.id)
+}
 
 function handleDayDelete(selectedDayId) {
   console.log(`TO DO delete func, id${selectedDayId}`)
